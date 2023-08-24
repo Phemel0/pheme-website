10a3a9a343051de0aa177b507fcbb07380887f2d
Ph3me <p.ntsimane@outlook.com>
2023-08-24 21:39:29 +0200

    First Initialize

---
+++ Components/header.tsx
@@ -0,0 +1,11 @@
+import  Image from "next/image";
+
+export default function header (){
+    <div className="">
+        <h1></h1>
+        <Image 
+        src=""
+        alt=""
+        />
+    </div>
+}
---
+++ src/components/hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function hero() {
+    return (
+    <div className=''>
+        <div className=''>
+            <h1>
+                This is Tagline
+            </h1>
+        </div>
+        <div>
+            <p></p>
+            <p></p>
+        </div>
+    </div>
+    )
+}
