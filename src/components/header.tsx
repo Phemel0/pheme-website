220f01c36ec395e34249d0bcdcc65ebd47d735da
Ph3me <p.ntsimane@outlook.com>
2023-08-26 20:00:44 +0200

    Hero Component Added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image"
 
 function header() {
     return (
-    <div>header</div>
+    <div>
+        <h1></h1>
+        <Image 
+            src="#placeholder " 
+            alt="placeholderImage" 
+        />
+        <h2></h2>
+        <p></p>
+    </div>
+
     )
 }
 
